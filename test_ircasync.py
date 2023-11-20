a5b66d8d1495bab27a0879cc35e62af5946b23ef
Kmiz07 <kmiz07@gmail.com>
2023-11-20 01:06:25 +0100

    Update test_ircasync.py

--- test_ircasync.py
+++ test_ircasync.py
@@ -10,6 +10,7 @@ async def evento(origen,mensaje):
 async def ciclo():
     uasyncio.create_task(irc_async.inicia_conexion(evento,"irc.freenode.org",6667,'uPython','#nmcuesp','micropython'))
     while True :
+        #aqui el codigo ciclico de programa
         await uasyncio.sleep(0)
 
 uasyncio.run(ciclo())
